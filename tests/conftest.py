c41e38366dc7745cd06734e99ae52deb767383a2
Darya Andronova <dbukhareva@yandex.ru>
2023-07-10 13:34:59 +0300

    Теперь тесты запускаются в фоновом режиме

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_management():
-    browser.config.window_height = 1080
-    browser.config.window_width = 1920
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless=new')
+    driver_options.add_argument("--window-size=1920,1080")
+    browser.config.driver_options = driver_options
 
     yield
 
